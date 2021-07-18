d952376135aec860f83fc428ffb987f8bf4dddcf
Dzmitry Marudau <Dzmitry_Marudau@epam.com>
2021-07-18 15:44:11 +0300

    Spring Boot Test Slice Annotations: @WebMvcTest

---
+++ src/test/java/by/dma/springboottesting/controller/QuestionControllerTest.java
@@ -0,0 +1,56 @@
+package by.dma.springboottesting.controller;
+
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import by.dma.springboottesting.domain.Question;
+import by.dma.springboottesting.service.QuestionService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(QuestionController.class)
+class QuestionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private QuestionService service;
+
+    @Test
+    void shouldReturnOK() throws Exception {
+        this.mockMvc.perform(get("/api/questions")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnListOfQuestions() throws Exception {
+        // given:
+        Question sample = new Question();
+        sample.setId(123L);
+        sample.setSortOrder(2);
+        sample.setName("What is your favorite social network?");
+        sample.setCreatedAt(ZonedDateTime.of(2020, 1, 1, 0, 0, 0, 0,
+                                             ZoneId.of("Europe/Minsk")));
+        Mockito.when(service.getAllQuestions()).thenReturn(List.of(sample));
+
+        // when:
+        var result = mockMvc.perform(get("/api/questions"));
+
+        //then:
+        result.andExpect(jsonPath("$.size()").value(1))
+              .andExpect(jsonPath("$[0].id").value(123))
+              .andExpect(jsonPath("$[0].name").value("What is your favorite social network?"))
+              .andExpect(jsonPath("$[0].createdAt").value("2020-01-01T00:00:00+03:00"));
+    }
+}
