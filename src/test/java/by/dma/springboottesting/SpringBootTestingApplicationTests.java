feaae05a36f95d3d63967c0539e37c5165c8d843
Dzmitry Marudau <Dzmitry_Marudau@epam.com>
2021-07-25 17:22:51 +0300

    Basic test containers setup

--- src/test/java/by/dma/springboottesting/SpringBootTestingApplicationTests.java
+++ src/test/java/by/dma/springboottesting/SpringBootTestingApplicationTests.java
@@ -2,12 +2,35 @@ package by.dma.springboottesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+//@SpringBootTest
 class SpringBootTestingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Container
+    static PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"))
+            .withDatabaseName("test")
+            .withUsername("spring-boot")
+            .withPassword("spring-boot");
 
+    // Spring boot > 2.2.6
+/*    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }*/
+
+    @Test
+    void shouldStartPostgreSQLDatabase() {
+        assertTrue(container.isRunning());
+        System.out.println("Context loads!");
+    }
 }
---
+++ src/test/resources/scripts/INIT_TEST_QUESTIONS.sql
@@ -0,0 +1,4 @@
+INSERT INTO question (id, name, created_at, sort_order) VALUES (1, 'What is your favorite color?', '2019-01-01 11:00:00+03', 2);
+INSERT INTO question (id, name, created_at, sort_order) VALUES (123, 'Do you like this interview?', '2018-01-01 11:00:00+01', 1);
+INSERT INTO question (id, name, created_at, sort_order) VALUES (2, 'Instagram or TikTok?', '2021-01-01 11:00:00+04', 3);
+INSERT INTO question (id, name, created_at, sort_order) VALUES (3, 'How to interview?', '2020-01-01 11:00:00+01', 1);
