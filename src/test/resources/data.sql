0ec0eec20c2ff3ce6b11faf8180aec30004a1b81
Dzmitry Marudau <dzmitry_marudau@epam.com>
2022-04-24 17:30:16 +0300

    Use a data.sql file to load initial data on application startup

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, name, joined_at) VALUES (11, 'duke', '2010-01-01 11:00:00+03');
+INSERT INTO customer (id, name, joined_at) VALUES (12, 'andy', '2020-01-01 11:00:00+01');
+INSERT INTO customer (id, name, joined_at) VALUES (13, 'anna', '2021-01-01 11:00:00+04');
+INSERT INTO customer (id, name, joined_at) VALUES (14, 'sandy', '2015-01-01 11:00:00+06');
+INSERT INTO customer (id, name, joined_at) VALUES (15, 'marc', '2001-01-01 11:00:00-06');
